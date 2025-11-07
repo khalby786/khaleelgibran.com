28941d65190a1e2b43836ec1eea26db1fbaa4215
Lexi Mattick <lexi.mattick@gmail.com>
2025-11-06 16:38:48 -0800

    Fix the webring

--- src/components/Webring.vue
+++ src/components/Webring.vue
@@ -1,10 +1,10 @@
 <template>
-  <!-- https://overengineering.kognise.dev/rand -->
-  <!-- https://overengineering.kognise.dev/ -->
+  <!-- To trick the aliveness checker: -->
+  <!-- "https://overengineering.kognise.dev/embed/khalby786" -->
   <div
     class="w-full h-24 border border-slate-400/25 shadow-lg p-5 flex flex-row gap-3.5 justify-center"
   >
-    <a :href="yamlData.previous" target="_parent">&laquo; prev</a>
+    <a :href="webringData.previous" target="_parent">&laquo; prev</a>
     <nav class="flex-1">  
       <p class="text-slate-200">khalby786 @ overengineeRING <span class="text-xs">(a webring)</span></p>
       <div>
@@ -12,33 +12,35 @@
         <a class="text-slate-400" href="https://overengineering.kognise.dev/rand">rand</a>
       </div>
     </nav>
-    <a :href="yamlData.next" target="_parent">next &raquo;</a>
+    <a :href="webringData.next" target="_parent">next &raquo;</a>
   </div>
 </template>
 
 <script>
-import YAML from "yaml";
-
 export default {
   data: function () {
     return {
-      yamlData: {
-        previous: "https://wavecat.xyz/",
-        next: "https://munvoseli.github.io/",
+      webringData: {
+        previous: "https://devrim.tech/",
+        next: "https://kognise.dev/",
       },
     };
   },
   mounted() {
-    fetch("https://cdn.jsdelivr.net/gh/kognise/overengineering/config.yaml")
-      .then((res) => res.text())
-      .then((data) => {
-        const parsedYaml = YAML.parse(data);
-        const myIndex = parsedYaml.sites.findIndex(
-          (site) => site.name === "khalby786"
+    fetch("https://overengineering.kognise.dev/members.json")
+      .then((res) => res.json())
+      .then((members) => {
+        const filteredMembers = member
+          .filter(
+            ([ member, health ]) => member.name === "khalby786" || health === "Ok"
+          )
+          .map(([ member ]) => member);
+        const myIndex = filteredMembers.findIndex(
+          (member) => member.name === "khalby786"
         );
-        const previousSite = parsedYaml.sites[myIndex - 1].url;
-        const nextSite = parsedYaml.sites[myIndex + 1].url;
-        this.yamlData = {
+        const previousSite = filteredMembers[myIndex - 1].url;
+        const nextSite = filteredMembers[myIndex + 1].url;
+        this.webringData = {
           previous: previousSite,
           next: nextSite,
         };
